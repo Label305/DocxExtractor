be6c3c3747a9c1529919ea649a4c7a3db883f3bb
Xander Peuscher <xander@label305.com>
2021-02-26 12:52:14 +0100

    Add support for w:ins element

---
+++ src/Label305/DocxExtractor/Decorated/Insertion.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Label305\DocxExtractor\Decorated;
+
+/**
+ * Class Insertion
+ * @package Label305\DocxExtractor\Decorated
+ *
+ * Represents the style contents of a <w:ins> object in the docx format.
+ */
+class Insertion {
+
+    /**
+     * @var string|null
+     */
+    public $id;
+    /**
+     * @var string|null
+     */
+    public $author;
+    /**
+     * @var string|null
+     */
+    public $date;
+
+    function __construct($id, $author, $date) {
+        $this->id = $id;
+        $this->author = $author;
+        $this->date = $date;
+    }
+
+    /**
+     * To docx xml string
+     *
+     * @return string
+     */
+    public function toDocxXML()
+    {
+        $value = '<w:ins';
+
+        if ($this->id !== null) {
+            $value .= ' w:id="' . $this->id . '" ';
+        }
+        if ($this->author !== null) {
+            $value .= ' w:author="' . $this->author . '" ';
+        }
+        if ($this->date !== null) {
+            $value .= ' w:date="' . $this->date . '" ';
+        }
+        $value .= ' xmlns:w="http://schemas.openxmlformats.org/wordprocessingml/2006/main">';
+
+        return $value;
+    }
+}
